f254145850e4d99c00bcf8e4fbdf89fee28d0b59
Matze <matze@local>
2016-06-21 23:18:58 +0200

    adapt to actual module template conventions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var controller = require('./app/controller');
 
@@ -9,10 +10,27 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  controller.get()
+
+  let pckg = fs.readFileSync('package.json');
+  pckg = JSON.parse(pckg);
+
+  let params = {};
+  params.data = true;
+  let widget = {};
+  widget._id = '1234567890';
+  widget.size = pckg.smartmirror.size[0];
+
+  controller.get(params)
     .then((videoURL) => {
-      res.render('app/view.jade', {videoURL: videoURL});
-    });
+      res.render('./index.jade', {
+        videoURL: videoURL,
+        widget: widget,
+      });
+    })
+    .catch((err) => {
+      console.dir(err);
+    res.status(500).send(err);
+  });
 });
 
 app.listen(8080, function () {
